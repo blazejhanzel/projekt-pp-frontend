857878fc5507d1d1ba6e1227f7758d25b0dad04a
Błażej Hanzel <blazej.hanzel@gmail.com>
2021-06-19 16:05:32 +0200

    add thread form updated

--- src/components/AddThreadForm.js
+++ src/components/AddThreadForm.js
@@ -3,8 +3,36 @@ import React, { useState, useEffect } from 'react'
 import { getCookie } from '../libraries/Cookie'
 
 function AddThreadForm(props) {
+    const [sections, setSections] = useState([])
+
+    useEffect(() => {
+        fetch("https://projekt-pp-backend.herokuapp.com/section", {
+            method: 'GET',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + getCookie("jwt")
+            }
+        }).then(res => res.json().then(data => {
+            if (res.status == 200) {
+                let result = []
+                for (let section of data) {
+                    result.push(<option value={section.id}>{section.name}</option>)
+                }
+                setSections(result)
+            }
+        }))
+    }, [])
+
     return (
-        <div></div>
+        <div id="addThreadForm">
+            <select id="section" name="sections">
+                {sections}
+            </select>
+            <input type="text" id="topic_filed" placeholder="Nazwa tematu" />
+            <textarea></textarea>
+            <button type="button">Dodaj wątek</button>
+        </div>
     )
 }
 
